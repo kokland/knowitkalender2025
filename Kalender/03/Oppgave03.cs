c7c0c09d66c5a997dcae9e994abc3d3d2b322529
Kim Økland <kim.okland@knowit.no>
2025-12-03 13:56:14 +0100

    refaktorert oppgave 3

--- Kalender/03/Oppgave03.cs
+++ Kalender/03/Oppgave03.cs
@@ -6,16 +6,18 @@ public static class Oppgave03
     {
         var linjer = File.ReadAllLines("03/input.txt");
 
-        var elementer = LastElementer(linjer);
-        var top3 = elementer.OrderBy(x => x.TotalScore).Take(3);
-        var bottom3 = elementer.OrderByDescending(x => x.TotalScore).Take(3);
-        var all = top3.Concat(bottom3).OrderByDescending(b => b.TotalScore);
-        var stringlist = all.Select(c => $"{c.Navn} {c.TotalScore}");
-        var result = string.Join(",", stringlist);
-        Console.WriteLine(result);
-
-
-
+        var elementer = LastElementer(linjer)
+            .OrderBy(a => a.TotalScore)
+            .ToList();
+        
+        var top3 = elementer.Take(3);
+        var bottom3 = elementer.TakeLast(3); 
+        var all = top3.Concat(bottom3)
+            .OrderByDescending(b => b.TotalScore)
+            .Select(b => b.StringValue)
+            .ToList();
+        
+        Console.WriteLine(string.Join(",", all));
     }
 
     private static List<Element> LastElementer(string[] linjer)
@@ -41,10 +43,10 @@ public static class Oppgave03
 
 public class Element
 {
-    public string Navn { get; set; }
+    public string? Navn { get; set; }
     public int Snill { get; set; }
     public int Slem { get; set; }
     public int Pepperkaker { get; set; }
-
-    public int TotalScore => (Snill * 50) - (Slem * 25) + (Pepperkaker * 15);
+    public int TotalScore => Snill * 50 - Slem * 25 + Pepperkaker * 15;
+    public string StringValue => $"{Navn} {TotalScore}";
 }
