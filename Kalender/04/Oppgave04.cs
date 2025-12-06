b4231b654109493faf9d9dd110aaf6593cb6c06e
Kim Økland <kim.okland@knowit.no>
2025-12-06 11:46:10 +0100

    oppgave 4 løst - bossoppgave

--- Kalender/04/Oppgave04.cs
+++ Kalender/04/Oppgave04.cs
@@ -1,67 +1,51 @@
-using System.Security.Cryptography;
+using System.Diagnostics;
 
 namespace Kalender._04;
 
 public class Oppgave04
 {
-    private const int StandardEnergi = 5;
+    private static string? data;
+    private static int etappe = 0;
 
     public static void Solve()
     {
-        var data = File.ReadAllText("04/input.txt");
-
+        data = File.ReadAllText("04/input.txt");
         var energi = 3000;
+
+
+        var prev1 = 0;
+        var prev2 = 0;
 
         for (var i = 0; i < data.Length; i++)
         {
-            if (energi <= 0)
+            var cost = data[i] switch
             {
-                continue;
+                'S' => 5,
+                'B' => 10,
+                'D' => 15,
+                'I' => 0,
+                'P' => 0,
+                _ => 0
+            };
+
+            if (cost > 0 && energi < cost)
+            {
+                break;
             }
 
-            var current = data[i];
-            switch (current)
-            {
-                case 'S':
-                    energi -= StandardEnergi;
-                    break;
-                case 'B':
-                    energi -= StandardEnergi * 2;
-                    break;
-                case 'D':
-                    energi -= StandardEnergi * 3;
-                    break;
-                case 'I':
-                    break;
-                case 'P':
-                    if (i <= 1) continue;
+            energi -= cost;
 
-                    var bruktEnergi = BrukteEnergiForrigeTo(data[i - 1], data[i - 2]);
-                    energi += bruktEnergi;
-                    break;
+            if (data[i] == 'P')
+            {
+                energi += prev1 + prev2;
             }
 
+            prev2 = prev1;
+            prev1 = cost;
 
-            Console.WriteLine("Energi igjen etter etappe nr " + i + " -> " + current + ": " + energi);
+            etappe++;
         }
-    }
 
-    private static int BrukteEnergiForrigeTo(char a, char b)
-    {
-        var sum = 0;
-        sum += BeregnEnergiForbruk(a);
-        sum += BeregnEnergiForbruk(b);
-        return sum;
-    }
-
-    private static int BeregnEnergiForbruk(char x)
-    {
-        return x switch
-        {
-            'S' => StandardEnergi,
-            'B' => StandardEnergi * 2,
-            'D' => StandardEnergi * 3,
-            _ => 0
-        };
+        Console.WriteLine($"{nameof(Oppgave04)}: {etappe * 10}");
     }
 }
